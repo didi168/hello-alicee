94ec28d707ba8b9f162ab7407d604c08d8c40bc5
Prozymax <maxwellprosper76@gmail.com>
2023-10-08 18:39:44 +0100

    another update

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+const path = require('path')
 const http = require('http')
 app.use(bodyParser.urlencoded({ extended: true }))
 const port =  { argsNumber: 4000, message: `App is listening at 4000` }
 http.createServer(app)
-app.use(express.static('public', { maxAge: '3d' }))
+// app.use(express.static('public', { maxAge: '3d' }))
+app.use(express.static(path.join(__dirname, 'public',)));
+
 
 //Calling all routes for main website
 const indexRouter = require('./routes/index')
