6b003c7d1166a02ed9d72c039bcb9e60c0f4dc57
Prozymax <maxwellprosper76@gmail.com>
2023-10-08 18:55:28 +0100

    as

--- App.js
+++ App.js
@@ -5,13 +5,13 @@ const path = require('path')
 const http = require('http')
 app.use(bodyParser.urlencoded({ extended: true }))
 const port =  { argsNumber: 4000, message: `App is listening at 4000` }
-http.createServer(app)
+// http.createServer(app)
 // app.use(express.static('public', { maxAge: '3d' }))
 app.use(express.static(path.join(__dirname, 'public',)));
 
 
 //Calling all routes for main website
 const indexRouter = require('./routes/index')
-app.use('/', indexRouter);
+app.use('./', indexRouter);
 
 app.listen(port.argsNumber, () => { console.log(port.message)} )
