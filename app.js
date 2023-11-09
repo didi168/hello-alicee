bc99b1bbbb54f84908240c1b0464f1f004ac1fc8
didi168 <divineoko2018@gmail.com>
2023-11-09 21:28:24 +0100

    port change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path')
 const http = require('http')
 app.use(bodyParser.urlencoded({ extended: true }))
-const port =  { argsNumber: 5500, message: `App is listening at 5500` }
+const port =  { argsNumber: 3000, message: `App is listening at 3000` }
 app.use(express.static(path.join(__dirname, 'public',)));
 
 const server = http.createServer(app)
